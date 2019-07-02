5cf1d23b381e9a7ceca9257f46073097b9ad7c96
Alex Colby <acolby@AlexColbyMacBookAir.local>
2019-07-02 10:22:07 +0100

    added basic framework for brute-force algortihm

--- Scrabble/AI.py
+++ Scrabble/AI.py
@@ -7,3 +7,7 @@ class AI(player):
     def makemove(self):
         # overwrites the player's makemove method preventing it from being used
         pass
+
+    def getmoves(self):
+        # move the brute force method to here
+        pass
