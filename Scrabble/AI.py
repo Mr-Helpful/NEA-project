ce699b53bb8c9b709e9cf7e00b76354789647ce1
Alex Colby <acolby@AlexColcBookAir.lan>
2019-07-19 14:58:18 +0100

    - added scoremoves method to AI to allow moves to be chosen  - added board object to allow the board to be saved

--- Scrabble/AI.py
+++ Scrabble/AI.py
@@ -4,10 +4,14 @@ class AI(player):
     def __init__(self):
         pass
 
-    def makemove(self):
+    def makemove(self,board):
         # overwrites the player's makemove method preventing it from being used
         pass
 
-    def getmoves(self):
+    def getmoves(self,board):
         # move the brute force method to here
+        pass
+
+    def scoremoves(self,moves,weighting):
+        # rates the moves found using their score and some basic heuristics
         pass
